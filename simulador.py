341472e762f5ff1d562434672d8d2e86c1f2885a
Ana Julia Vieira <129344535+vieiranaju@users.noreply.github.com>
2024-06-01 19:48:37 +0000

    Comentarios

--- simulador.py
+++ simulador.py
@@ -10,19 +10,29 @@ def lerCsv(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter=';')
         return [row for row in reader]
+    #Retorna uma lista com as palavras 
 
 def simular(automato, input_string):
     estados_ativos = {automato['initial']}
+
+    #Para cada simbolo lido cria um conjunto de estados ativos
     for read in input_string:
         novos_estados_ativos = set()
+
+        #Verifica as possiveis transições
         for estado in estados_ativos:
             for transition in automato['transitions']:
                 if transition['from'] == estado and transition['read'] == read:
                     novos_estados_ativos.add(transition['to'])
+                    #Adiciona o novo estado no conjunto caso seja possivel atualizar
+
         if not novos_estados_ativos:
             return 0  
         estados_ativos = novos_estados_ativos
+        #Novos estados passam a ser o estado atual pro proximo simbolo
+
     return 1 if any(state in automato['final'] for state in estados_ativos) else 0
+    #No final da leitura, se algum estado na lista de estados atuais for final retorna 0
 
 def main():
     automatos = ['Exemplos/ex1.json', 'Exemplos/ex2.json', 'Exemplos/ex3.json']
@@ -32,6 +42,7 @@ def main():
         print(f"{aut_file} e {input_file}")
         aut = lerJson(aut_file)
         entradas = lerCsv(input_file)
+        #Lê cada um dos arquivos e entradas
 
         start_time = time.time()
 
@@ -40,6 +51,7 @@ def main():
             expected = int(row[1])
             aceita = simular(aut, input_string)
             print(f"Entrada: {input_string} - Aceita: {aceita} - Esperada: {expected}")
+            #Verifica cada elemento da lista de palavras e simula o automato correspondente
 
         end_time = time.time()
         elapsed_time = end_time - start_time
